5540ef097f7f9aca3f7e0e900c307c75f55ea0f2
waitingforlove24 <89089089z@gmail.com>
2024-07-28 02:30:34 +0300

    modified:   src/app/app-routing.module.ts 	deleted:    src/app/item/item-stats/item-stats.component.css 	deleted:    src/app/item/item-stats/item-stats.component.html 	deleted:    src/app/item/item-stats/item-stats.component.spec.ts 	deleted:    src/app/item/item-stats/item-stats.component.ts 	deleted:    src/app/plus-five.pipe.spec.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemComponent } from './item/item.component';
 import { ItemDetailsComponent } from './item/item-details/item-details.component';
-import { ItemStatsComponent } from './item/item-stats/item-stats.component';
 import { MainComponent } from './main/main.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
@@ -28,10 +27,6 @@ const routes: Routes = [
       {
         path: 'details',
         component: ItemDetailsComponent
-      },
-      {
-        path: 'stats',
-        component: ItemStatsComponent
       },
       {
         path: 'list',
--- src/app/plus-five.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PlusFivePipe } from './plus-five.pipe';
-
-describe('PlusFivePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PlusFivePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
